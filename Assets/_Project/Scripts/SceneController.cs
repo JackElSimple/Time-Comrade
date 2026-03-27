5e6446d9cf190f3a0eb93acf130145140c0f2dd2
michalRGB <>
2026-03-27 12:22:15 +0100

    emit event when time runs out

--- Assets/_Project/Scripts/SceneController.cs
+++ Assets/_Project/Scripts/SceneController.cs
@@ -45,7 +45,7 @@ public class SceneController : MonoBehaviour
             {
                 isRecording = false;
                 LoadState();
-
+                notifyListenersStop();
             }
         }
 
