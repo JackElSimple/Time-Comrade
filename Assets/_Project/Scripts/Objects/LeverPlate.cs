e6da14361af75dee436eeb6c37add445a9468b42
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-04-19 22:32:04 +0200

    leverPlate implemented

---
+++ Assets/_Project/Scripts/Objects/LeverPlate.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeverPlate: MovingPlatformBase
+{
+    [Header("Events")]
+    public UnityEvent on;
+    public UnityEvent off;
+
+    private bool pressed = false;
+    private bool doublePressed = false;
+    private bool playerOnPlate = false;
+    private bool active = false;
+    protected override void Awake()
+    {
+        base.Awake();
+
+        currentTarget = transform.position;
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if (targetPoint == null) return;
+
+        currentTarget = playerOnPlate ? targetPoint.position : startPos;
+
+        Move();
+    }
+
+    protected override void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!(collision.transform.CompareTag("Player") || collision.transform.CompareTag("Clone"))) return;
+
+        if (pressed)
+            doublePressed = true;
+        else
+        {
+            doublePressed = false;
+            pressed = true;
+            playerOnPlate = true;
+            HandlePassenger(collision.transform, true); // parent player
+            if (active) {
+                on?.Invoke();
+            }
+            else
+            {
+                off?.Invoke();
+            }
+        }
+    }
+
+    protected override void OnCollisionExit2D(Collision2D collision)
+    {
+        if (!(collision.transform.CompareTag("Player") || collision.transform.CompareTag("Clone"))) return;
+
+        if (doublePressed)
+            doublePressed = false;
+        else
+        {
+            pressed = false;
+            playerOnPlate = false;
+            HandlePassenger(collision.transform, false); // unparent player
+            active = !active;
+        }
+    }
+}
