90582efa104a30727d9b701393be2bb32ad4da98
Vthot2004 <65424747+Vthot2004@users.noreply.github.com>
2026-05-10 17:15:45 +0200

    lanzar confetti victoryPoint

--- Assets/_Project/Scripts/VictoryController.cs
+++ Assets/_Project/Scripts/VictoryController.cs
@@ -1,22 +1,39 @@
 using UnityEngine;
-
+using System.Collections;
 public class VictoryController : MonoBehaviour
 {
 	[Header("Selecciona la escena del menú")]
 	[SceneName] 
 	[SerializeField] private string nextSceneName;
 
-	private void OnTriggerEnter2D(Collider2D collision)
+	private ParticleSystem part;
+    private void Awake()
+    {
+        part = GetComponent<ParticleSystem>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Player"))
 		{
+
 			SceneController sc = FindFirstObjectByType<SceneController>();
-			sc.ReproducirTerminarNivel();
-			if (sc != null)
-			{
-				// Aquí usamos el string que el desplegable rellenó por nosotros
-				sc.CompleteLevel(nextSceneName);
-			}
-		}
+			part.Emit(50);
+            if (sc != null)
+            {
+                sc.ReproducirTerminarNivel();
+            }
+            StartCoroutine(WaitAndComplete(1.5f));
+        }
 	}
+    IEnumerator WaitAndComplete(float segundos)
+    {
+
+        yield return new WaitForSeconds(segundos);
+        SceneController sc = FindFirstObjectByType<SceneController>();
+        if (sc != null)
+        { 
+            // Aquí usamos el string que el desplegable rellenó por nosotros
+            sc.CompleteLevel(nextSceneName);
+        }
+    }
 }
